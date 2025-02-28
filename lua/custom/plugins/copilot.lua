e407a17a6e535f7698868ee08d81576e02d4d85d
juanmmartinss <juanmmartinss@outlook.com>
2025-02-28 18:24:07 -0300

    feat: copilot adjustments

--- lua/custom/plugins/copilot.lua
+++ lua/custom/plugins/copilot.lua
@@ -1,6 +1,7 @@
 return {
   "github/copilot.vim",
   config = function()
-    -- require("copilot").setup()
-  end
+    vim.g.copilot_no_tab_map = true -- Desabilita o mapeamento padrão do <Tab>
+    vim.api.nvim_set_keymap("i", "<C-J>", 'copilot#Accept("<CR>")', { expr = true, silent = true })
+  end,
 }

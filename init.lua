c2fdd61f276bed124b88d0441a67a5051edc5fed
juanmmartinss <juanmmartinss@outlook.com>
2025-02-28 21:20:34 -0300

    feat: keymap

--- init.lua
+++ init.lua
@@ -85,6 +85,7 @@ I hope you enjoy your Neovim journey,
 P.S. You can delete this when you're done too. It's your config now! :)
 --]]
 --
+require 'keymap'
 vim.g.loaded_netrw = 1
 vim.g.loaded_netrwPlugin = 1
 
@@ -173,6 +174,8 @@ vim.keymap.set('n', '<leader>q', vim.diagnostic.setloclist, { desc = 'Open diagn
 -- Exit terminal mode in the builtin terminal with a shortcut that is a bit easier
 -- for people to discover. Otherwise, you normally need to press <C-\><C-n>, which
 -- is not what someone will guess without a bit more experience.
+--
+vim.keymap.set('n', '<leader>t', ':terminal<CR>', { noremap = true, silent = true })
 --
 -- NOTE: This won't work in all terminal emulators/tmux/etc. Try your own mapping
 -- or just use <C-\><C-n> to exit terminal mode
---
+++ lua/keymap.lua
@@ -0,0 +1,10 @@
+local map = vim.api.nvim_set_keymap
+local opts = { noremap = true, silent = true }
+
+-- Salvar com Ctrl + S
+map('n', '<C-s>', ':w<CR>', opts)
+map('i', '<C-s>', '<Esc>:w<CR>a', opts)
+map('v', '<C-s>', '<Esc>:w<CR>', opts)
+
+-- Fechar janela com Ctrl + Q
+map('n', '<C-q>', ':q<CR>', opts)

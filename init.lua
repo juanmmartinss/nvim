8b358d9df456622c3d4cd5c4767e1b058ca07fcc
juanmmartinss <juanmmartinss@outlook.com>
2025-03-04 22:36:07 -0300

    feat: ctags

--- init.lua
+++ init.lua
@@ -90,6 +90,8 @@ require 'keymap'
 vim.g.loaded_netrw = 1
 vim.g.loaded_netrwPlugin = 1
 
+vim.opt.tags = './tags;,tags;'
+
 -- Set <space> as the leader key
 -- See `:help mapleader`
 --  NOTE: Must happen before plugins are loaded (otherwise wrong leader will be used)

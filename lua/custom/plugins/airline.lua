985f4f2353ace9ae64227350aaba9843c3e7b24f
juanmmartinss <juanmmartinss@outlook.com>
2025-03-26 08:23:06 -0300

    fix: airline theme

--- lua/custom/plugins/airline.lua
+++ lua/custom/plugins/airline.lua
@@ -1,9 +1,13 @@
 return {
-  'vim-airline/vim-airline',
-  'vim-airline/vim-airline-themes',
-  config = function()
-    -- Configuração do vim-airline aqui
-    vim.g['airline#extensions#tabline#enabled'] = 1  -- Habilita a aba de navegação
-    vim.g['airline#extensions#fugitive#enabled'] = 1  -- Ativa o plugin Fugitive se usado
-  end
+  {
+    'vim-airline/vim-airline',
+    dependencies = { 'vim-airline/vim-airline-themes' },
+    config = function()
+      -- Configuração do vim-airline aqui
+      vim.g['airline#extensions#tabline#enabled'] = 1  -- Habilita a aba de navegação
+      vim.g['airline#extensions#fugitive#enabled'] = 1 -- Ativa o plugin Fugitive se usado
+      vim.g.airline_theme = 'base16_gruvbox_dark_hard' -- Define o tema Gruvbox para o Airline
+    end
+  }
 }
+

7ab99ccd3dfffcc243698216be70cd7885c166c8
juanmmartinss <juanmmartinss@outlook.com>
2025-08-20 14:15:57 -0300

    feat: neogen

--- lua/custom/plugins/airline.lua
+++ lua/custom/plugins/airline.lua
@@ -6,7 +6,8 @@ return {
       -- Configuração do vim-airline aqui
       vim.g['airline#extensions#tabline#enabled'] = 1  -- Habilita a aba de navegação
       vim.g['airline#extensions#fugitive#enabled'] = 1 -- Ativa o plugin Fugitive se usado
-      vim.g.airline_theme = 'base16_gruvbox_dark_hard' -- Define o tema Gruvbox para o Airline
+      -- vim.g.airline_theme = 'base16_gruvbox_dark_hard' -- Define o tema Gruvbox para o Airline
+      vim.g.airline_theme = 'base16_default_dark' -- Define o tema Gruvbox para o Airline
     end
   }
 }
--- lua/custom/plugins/color_scheme.lua
+++ lua/custom/plugins/color_scheme.lua
@@ -1,12 +1,47 @@
+-- return {
+--   'ellisonleao/gruvbox.nvim',
+--   priority = 1000,
+--   config = function()
+--     require("gruvbox").setup({
+--       contrast = "hard", -- Opções: "soft", "medium", "hard"
+--       transparent_mode = false,
+--     })
+--     vim.o.background = 'dark' -- Define fundo escuro
+--     vim.cmd("colorscheme gruvbox")
+--   end,
+-- }
+
 return {
-  'ellisonleao/gruvbox.nvim',
+  "rebelot/kanagawa.nvim",
   priority = 1000,
   config = function()
-    require("gruvbox").setup({
-      contrast = "hard", -- Opções: "soft", "medium", "hard"
-      transparent_mode = false,
+    require("kanagawa").setup({
+      compile = false,             -- enable compiling the colorscheme
+      undercurl = true,            -- enable undercurls
+      commentStyle = { italic = true },
+      functionStyle = {},
+      keywordStyle = { italic = true },
+      statementStyle = { bold = true },
+      typeStyle = {},
+      transparent = false,         -- do not set background color
+      dimInactive = false,         -- dim inactive window
+      terminalColors = true,       -- define vim.g.terminal_color_{0,17}
+      colors = {
+        palette = {},
+        theme = { wave = {}, lotus = {}, dragon = {}, all = {} },
+      },
+      overrides = function(colors) -- custom highlight tweaks
+        return {}
+      end,
+      theme = "dragon",              -- default theme
+      background = {
+        dark = "dragon",
+        light = "dragon"
+      },
     })
-    vim.o.background = 'dark' -- Define fundo escuro
-    vim.cmd("colorscheme gruvbox")
+
+    -- setup must be called before loading
+    vim.cmd("colorscheme kanagawa")
   end,
 }
+
